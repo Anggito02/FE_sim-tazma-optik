48bde0129373c537edf27fba711f927bb5cf371f
abhiramadamas <muhammaddamas81@gmail.com>
2023-12-20 14:37:21 +0700

    [TESTING]

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller{
             ];
             
             $response = Http::withHeaders($headers)->post($_ENV['BACKEND_API_ENDPOINT'].'/login', $api_request);
-            dd($response);
+            // dd($response);
             
             $data = $response->json();
 
