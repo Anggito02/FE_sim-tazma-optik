e5f5965bd124820e5c975fde3417bea078149c0b
abhiramadamas <muhammaddamas81@gmail.com>
2024-03-14 14:18:27 +0700

    [FIX] layout username data

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -210,7 +210,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline black-text small">damas</span>
+                                <span class="mr-2 d-none d-lg-inline black-text small">{{ $data['username'] }}</span>
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
