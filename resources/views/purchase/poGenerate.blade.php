509006c3780566bc83b82c52581a56774a877bb4
abhiramadamas <muhammaddamas81@gmail.com>
2024-04-01 23:34:03 +0700

    [FIX] po generate blade

--- resources/views/purchase/poGenerate.blade.php
+++ resources/views/purchase/poGenerate.blade.php
@@ -1,23 +1,15 @@
-<style>
-  .chosen-container .chosen-results {
-      font-size: 14px; /* Sesuaikan ukuran font */
-  }
-
-</style>
-<span id="tambah_info"></span>
 <div class="modal-body">
   <div id="qrcode">
     <div class="d-flex flex-column align-items-center">
       <div class="mb-4">
         {{ QrCode::size(250)->generate($qr_pod['kode_qr_po_detail']) }}
       </div>
+      <div class="mb-1">
+        <p class="black-text">{{$qr_pod['kode_item']}}</p>
+      </div>
       <div class="mb-3">
-        <p class="black-text">Kode Item = {{$qr_pod['kode_item']}}</p>
+        <p class="black-text">{{$qr_pod['kode_qr_po_detail']}}</p>
       </div>
     </div>
   </div>
 </div>
-
-<script type="text/javascript">
-
-</script>
